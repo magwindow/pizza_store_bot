61f9ca050c4ef485ee67c199422ae597d5ab1c9f
magwindow <antosha.petrakov@yandex.ru>
2025-08-24 11:44:47 +0300

    Added custom filters

--- handlers/user_group.py
+++ handlers/user_group.py
@@ -2,7 +2,10 @@ from string import punctuation
 
 from aiogram import Router, types
 
+from filters.chat_types import ChatTypeFilter
+
 user_group_router = Router()
+user_group_router.message.filter(ChatTypeFilter(['group', 'supergroup']))
 
 restricted_words = {'кабан', 'хомяк', 'выхухоль'}
 
@@ -11,8 +14,9 @@ def clean_text(text: str):
     return text.translate(str.maketrans('', '', punctuation))
 
 
+@user_group_router.edited_message()
 @user_group_router.message()
 async def cleaner(message: types.Message):
-    if restricted_words.intersection(message.text.lower().split()):
+    if restricted_words.intersection(clean_text(message.text.lower()).split()):
         await message.answer(f'{message.from_user.first_name}, соблюдайте порядок в чате!')
         await message.delete()
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,7 +1,10 @@
 from aiogram import types, Router
 from aiogram.filters import CommandStart, Command
 
+from filters.chat_types import ChatTypeFilter
+
 user_private_router = Router()
+user_private_router.message.filter(ChatTypeFilter(['private']))
 
 
 @user_private_router.message(CommandStart())
