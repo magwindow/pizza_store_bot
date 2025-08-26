4320796bb4c7cfd00e32734b70aaaea08455526d
magwindow <antosha.petrakov@yandex.ru>
2025-08-26 18:58:09 +0300

    Added an update to the main menu when you click on the inline buttons: About us, Payment, Delivery

--- handlers/user_private.py
+++ handlers/user_private.py
@@ -3,8 +3,8 @@ from aiogram.filters import CommandStart, Command
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from filters.chat_types import ChatTypeFilter
-from keyboards.inline import get_callback_btns
 from handlers.menu_processing import get_menu_content
+from keyboards.inline import MenuCallBack
 
 user_private_router = Router()
 user_private_router.message.filter(ChatTypeFilter(['private']))
@@ -16,9 +16,13 @@ async def start_cmd(message: types.Message, session: AsyncSession):
     await message.answer_photo(media.media, caption=media.caption, reply_markup=reply_markup)
 
 
+@user_private_router.callback_query(MenuCallBack.filter())
+async def user_menu(callback: types.CallbackQuery, callback_data: MenuCallBack, session: AsyncSession):
+    media, reply_markup = await get_menu_content(session, level=callback_data.level, menu_name=callback_data.menu_name)
+    await callback.message.edit_media(media=media, reply_markup=reply_markup)
+    await callback.answer()
+
+
 @user_private_router.message(Command('help'))
 async def about_cmd(message: types.Message):
     await message.answer('Помощь по навигации в боте')
-
-
-
